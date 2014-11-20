dd4b6444a21a12e80213d3039a399d9ccf269584
Kushal Pradhan <pkushal@uab.edu>
2014-11-19 21:47:33 -0600

    Python notes

--- PyCharm/Python_Kushal/FirstPython.py
+++ PyCharm/Python_Kushal/FirstPython.py
@@ -1,9 +1,15 @@
-magicNumber = 12
+import requests
+from bs4 import BeautifulSoup
 
-for x in range(1,101):
-    if x is magicNumber:
-        print("magic number is ", x, " wala")
-        break
-    else:
-        print(x)
+def trade_spider(max_pages):
+    page=1
+    while page < max_pages:
+        url = 'https://buckysroom.org/trade/search.php?page='+str(page)
+        source_code = requests.get(url)  # gets the source code
+        plain_text = source_code.text  # extracts the plain text only
+
+
+
+
+
 
